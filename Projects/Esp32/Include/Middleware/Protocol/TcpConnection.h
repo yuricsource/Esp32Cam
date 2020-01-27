d4ad017ad5cee4d4e69a17abc5479b97e3689527
Yuri Rodrigues <yuri.flash@gmail.com>
2020-01-28 00:07:44 +1100

    Implementing TCP connection interacting with DNS Client and connection to the server.

--- Projects/Esp32/Include/Middleware/Protocol/TcpConnection.h
+++ Projects/Esp32/Include/Middleware/Protocol/TcpConnection.h
@@ -4,17 +4,23 @@
 #include "TimeLimit.h"
 #include "lwip/tcp.h"
 #include "DnsClient.h"
+#include "Logger.h"
+
 namespace Middleware 
 {
 namespace Protocol {
 
 using Hal::TimeLimit;
+using Hal::Hardware;
+// using Middleware::BaseConnection::ConnectStatus;
+using Utilities::Logger;
 
 class TcpConnection : public BaseConnection
 {
 public:
 	TcpConnection() : 
-		BaseConnection()
+		BaseConnection(),
+		_port(0), _pcb(nullptr)
 	{
 	}
 	
@@ -22,7 +28,22 @@ public:
 	{
 		Close();
 	}
+
+	uint16_t GetPort();
+
 private:
+
+	static constexpr uint16_t StartPort = 49152;
+	uint16_t _port;
+
+	tcp_pcb *_pcb;
+
+	static err_t receiveHandler(void *arg, struct tcp_pcb *pcb, struct pbuf *p, err_t err);
+	static void clearPcbHandler(TcpConnection* conn, tcp_pcb *pcb);
+	static err_t pollHandler(void *arg, struct tcp_pcb *pcb);
+	static void errorHandler(void *arg, err_t err);
+	static err_t sentHandler(void *arg, struct tcp_pcb *pcb, u16_t len);
+	static err_t connectedHandler(void *arg, struct tcp_pcb *pcb, err_t err);
 
 	BaseConnection::ConnectStatus DoConnect(const RemoteConnection &remoteConnection) override;
 
@@ -33,6 +54,7 @@ private:
 	void DoReset() override;
 
 	bool IsReady() override;
+	
 private:
 	/// @brief	Hide Copy constructor.
 	TcpConnection(const TcpConnection&) = delete;
